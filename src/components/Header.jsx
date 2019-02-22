e441f7d4c754e0348108c28361a84c0e33c75010
Parker Couch <parkercouch@gmail.com>
2019-02-22 13:31:12 -0800

    Updated slider styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,17 +3,17 @@ import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 
 const Header = (props) => { 
-  const selected = "f4 lh-copy h3-5 ttu fw5 link dim ba bw1 ph3 pb3 pt3 mb2 dib black bg-white pointer selected relative shadow-5";
-  const notSelected = "f4 lh-copy h3 ttu fw5 link dim ph3 pb3 pt3 mb2 dib white bg-black pointer";
+  const selected = "f5 h2-5 lh-title ttu fw5 link dim ba bw1 ph3 pb3 pt1 mb2 dib black bg-white pointer selected relative shadow-5";
+  const notSelected = "f5 h2 ttu fw5 link dim ph3 pb3 pt1 mb2 dib white bg-black pointer";
 
   return (
-    <header className="bg-white tc">
+    <header className="bg-white tc ma0 pa0">
       <h1
-        className="f1"
+        className="f2 mt2 mb3 mh0 pa0"
       >
-        Countries Impact On the World
+        Developing World
       </h1>
-      <nav className="nav bg-black db h3 w-100 mb2">
+      <nav className="nav bg-black db h2-2 w-100 mb3">
         {props.mapNames.map((title, i) => (
           <div
             className={i === props.currentMap ? selected : notSelected}
@@ -24,9 +24,9 @@ const Header = (props) => {
           </div>
         ))}
       </nav>
-      <div className="bg-white w-100 pa0 ma0">
+      <div className="bg-white w-100 pa0 pb4 ma0 shadow-5">
         <div className="dib w-100 pa0 ma0">
-          <p className="dib b pa0 ma0">1990</p> 
+          <p className="dib b pa0 ma0">1994</p> 
           <Slider
             className="dib measure ba br-pill bw1 mh3 mt2"
             onChange={props.handleChangeYear}
